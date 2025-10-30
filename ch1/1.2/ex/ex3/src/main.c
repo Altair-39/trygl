7ca58c3cf14000907c1d37178a2282a4061dd5ec
altair <luca.barra@edu.unito.it>
2025-10-30 11:34:16 +0100

    1.2 - ex

---
+++ ch1/1.2/ex/ex3/src/main.c
@@ -0,0 +1,231 @@
+#include "../include/glad/glad.h"
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// Shaders
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *fragmentShaderSource1 =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+const char *fragmentShaderSource2 =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+    "}\n\0";
+
+// Whenever the window changes in size, GLFW calls this function
+// and fills in the proper arguments for you to process.
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+// Input: ESC close the window
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, 1);
+}
+
+int main() {
+  // Initialize GLFW
+  glfwInit();
+
+  // It lock to the version 3 of opengl
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+  // We want to use the core profile (smaller subset)
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Creation of a window object
+  GLFWwindow *window =
+      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Silksong", NULL, NULL);
+
+  if (window == NULL) {
+    printf("Failed to create GLFW window");
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  // We call GLAD before everything else
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    printf("Failed to initialize GLAD");
+    return -1;
+  }
+
+  // Tell opengl the size of the rendering
+  glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+  // Callback function
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // vertices of the triangles, the z is 0 to make it 2D
+  float vertices1[] = {
+      // first triangle
+      -0.9f,  -0.5f, 0.0f, // left
+      -0.0f,  -0.5f, 0.0f, // right
+      -0.45f, 0.5f,  0.0f, // top
+  };
+  float vertices2[] = {
+      // second triangle
+      0.0f,  -0.5f, 0.0f, // left
+      0.9f,  -0.5f, 0.0f, // right
+      0.45f, 0.5f,  0.0f  // top
+  };
+
+  // VERTEX ARRAY OBJECT (VAO): contains the vertex of the objects
+  unsigned int VAO1, VAO2;
+  glGenVertexArrays(1, &VAO1);
+  glGenVertexArrays(1, &VAO2);
+
+  // Vertex Buffer Object (VBO): necessary for sending data to the GPU
+  unsigned int VBO1, VBO2;
+  glGenBuffers(1, &VBO1);
+  glGenBuffers(1, &VBO2);
+
+  // Setup first triangle (VAO1 + VBO1)
+  glBindVertexArray(VAO1);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  // Setup second triangle (VAO2 + VBO2)
+  glBindVertexArray(VAO2);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  // Create a shader object
+  unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+  // Compile the shader
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  // Check for failure in shader's compilation
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n");
+    printf("%s", infoLog);
+  }
+
+  // Second shader
+  unsigned int fragmentShader1 = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader1, 1, &fragmentShaderSource1, NULL);
+  glCompileShader(fragmentShader1);
+  glGetShaderiv(fragmentShader1, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragmentShader1, 512, NULL, infoLog);
+    printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n");
+    printf("%s", infoLog);
+  }
+
+  // Create a shader program
+  unsigned int shaderProgram1 = glCreateProgram();
+
+  // Attach the shaders to the program
+  glAttachShader(shaderProgram1, vertexShader);
+  glAttachShader(shaderProgram1, fragmentShader1);
+  glLinkProgram(shaderProgram1);
+  glGetProgramiv(shaderProgram1, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram1, 512, NULL, infoLog);
+    printf("ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n");
+    printf("%s", infoLog);
+  }
+
+  unsigned int fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
+  glCompileShader(fragmentShader2);
+  glGetShaderiv(fragmentShader2, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragmentShader2, 512, NULL, infoLog);
+    printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n");
+    printf("%s", infoLog);
+  }
+
+  // Create a shader program
+  unsigned int shaderProgram2 = glCreateProgram();
+
+  // Attach the shaders to the program
+  glAttachShader(shaderProgram2, vertexShader);
+  glAttachShader(shaderProgram2, fragmentShader2);
+  glLinkProgram(shaderProgram2);
+  glGetProgramiv(shaderProgram2, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram2, 512, NULL, infoLog);
+    printf("ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n");
+    printf("%s", infoLog);
+  }
+
+  // After the linking we don't need the shader anymore
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader1);
+  glDeleteShader(fragmentShader2);
+
+  // Tell opengl how interpret the vertex data
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  // Render loop
+  while (!glfwWindowShouldClose(window)) {
+    // Clear at the start of every iteration
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Activate the shader
+    glUseProgram(shaderProgram1);
+
+    // Draw first triangle
+    glBindVertexArray(VAO1);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // Activate the shader
+    glUseProgram(shaderProgram2);
+
+    // Draw second triangle
+    glBindVertexArray(VAO2);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // Check for input
+    processInput(window);
+    // Swap the color of the buffer
+    glfwSwapBuffers(window);
+
+    // CHeck for events
+    glfwPollEvents();
+  }
+
+  // Free all resources
+  glDeleteVertexArrays(1, &VAO1);
+  glDeleteBuffers(1, &VBO1);
+  glDeleteVertexArrays(1, &VAO2);
+  glDeleteBuffers(1, &VBO2);
+  glDeleteProgram(shaderProgram1);
+  glDeleteProgram(shaderProgram1);
+  glfwTerminate();
+
+  return 0;
+}
